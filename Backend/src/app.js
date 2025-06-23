da8d434d441107eff37e64e7b888c1954fae4f84
Aayush <sthaaayush375@gmail.com>
2025-06-23 13:36:06 +0545

    Some change in user controller and model

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,10 +1,12 @@
 import express from "express";
 import userRouter from "./routes/user.routes.js"
 import path from "path"
+import cookieParser from "cookie-parser"
 const app = express();
  
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
+app.use(cookieParser())
 app.use(express.static(path.resolve("../frontend")));
 
 app.use("/user", userRouter)
