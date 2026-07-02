05950f98ec44ae8cf55859cba54bbfc755684a78
Aayush shrestha <sthaaayush375@gmail.com>
2026-07-02 17:27:16 +0545

    fix: update CORS origin to allow all origins dynamically

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -12,7 +12,14 @@ import path from "path"
 import cookieParser from "cookie-parser"
 import paymentRouter from "./routes/payment.routes.js";
 const app = express();
- 
+import cors from "cors";
+const corsOptions = {
+  origin: true,
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
 app.use(express.static("public"));
